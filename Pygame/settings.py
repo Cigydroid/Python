bb856192b1a717b56a5b9cf3239b789e79db0831
Cigydroid <brucekv@spskladno.cz>
2026-03-04 09:27:59 +0100

    change

--- Pygame/settings.py
+++ Pygame/settings.py
@@ -1,18 +1,22 @@
+#game settings
 WIDTH = 500
 HEIGHT = 800
-PFS = 30
+FPS = 30
 
+#player settings
 PLAYER_WIDTH = 50
 PLAYER_HEIGHT = 50
 PLAYER_SPEED = 5
-PLAYER_IMAGE_PATH = "guide.png"
+PLAYER_IMAGE_PATH = "obrazky/spaceship.png"
 
+#colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 ORANGE = (255, 165, 0)
 YELLOW = (255, 255, 0)
 
-BLOCK_WIDTH = 50
+#block settings
+BLOCK_WIDTH = 70
 BLOCK_HEIGHT = 50
+BLOCK_IMAGE_PATH = "obrazky/ground.png"
 BLOCK_SPEED = 5
-BLOCK_IMAGE_PATH = "george.jfif"
